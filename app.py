b068b15076729d22d0c214b1c256b56c965fd05b
ElleBoii <saile324@gmail.com>
2026-04-05 14:25:59 +0200

    Create database tables on startup

--- app.py
+++ app.py
@@ -686,7 +686,8 @@ def save_klockan_session():
     return redirect(url_for("index"))
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
